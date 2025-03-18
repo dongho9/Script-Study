e4877076119847a3604d9ea912e64c5bae84063c
강동호 <donghobridge@gmail.com>
2025-03-18 18:05:42 +0900

    250318-01

---
+++ 250318/tailwind.config.js
@@ -0,0 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {
+//       fontfamily: {
+//         sans: ["Noto Sans", sans - serif],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        notosans: ["Noto Sans", "serif"],
+        single: ["Single Day", "cursive"],
+      },
+    },
+  },
+};
